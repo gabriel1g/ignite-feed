4ba7e732684ce36a98d329fa92854111af4f1736
gabrielrodrigues <gabriel.grf8395@gmail.com>
2023-04-04 13:16:29 -0300

    sidebar da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 import './global.css';
 
 export default function App() {
-  return <></>;
+  return (
+    <div>
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error optio dolor animi facilis nemo, quasi earum officia mollitia nisi
+          molestiae, dolores qui iure et, consequuntur laborum id ullam harum corporis.
+        </main>
+      </div>
+    </div>
+  );
 }
